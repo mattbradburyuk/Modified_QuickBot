981d4f0881746f13459596c91907af60782ef4bd
root <root@beaglebone.localdomain>
2015-03-01 21:10:14 +0000

    new encoder reader appears to be working, next step load onto robot

--- QB2.py
+++ QB2.py
@@ -25,6 +25,8 @@ import socket
 import threading
 import numpy as np
 
+import pypru as pp  # start_up_pru(), stop_pru(), get_pru_data()
+
 import Adafruit_BBIO.GPIO as GPIO
 import Adafruit_BBIO.PWM as PWM
 import Adafruit_BBIO.ADC as ADC
@@ -34,6 +36,8 @@ LEFT = 0
 RIGHT = 1
 MIN = 0
 MAX = 1
+
+MAP_SIZE = 4096
 
 DEBUG = False
 
@@ -54,6 +58,24 @@ class QuickBot():
     # Parameters
     sampleTime = 20.0 / 1000.0
 
+    # timing 
+    sampleTime = 20.0/1000.0        #sample time of the update function
+    pruCurrentTime = 0.0    # time of current last sample per pru
+    pruPrevTime = 0.0       # time of previous last sample per pru
+    pruTotalTime = 0.0      # total time since pru started per pru
+    sysStartTime = 0.0      # system time when pru started
+    sysTotalTime = 0.0      # total time since pru started per system
+
+    # encoder buffer
+    bufferLoc = 4    # initialise at start of sample data in buffer
+    prevBufferLoc = 0
+
+    #ticks
+    prevLastTick = [0,0];
+    runningAverage = [0.0,0.0];
+    tickVel = [0,0]
+
+
     # Pins
     ledPin = 'USR1'
 
@@ -69,8 +91,6 @@ class QuickBot():
     # Encoder counting parameter and variables
     ticksPerTurn = 4  # Number of ticks on encoder disc
     minPWMThreshold = [45, 45]  # Threshold on the minimum value to turn wheel
-
-# add new
 
 
 
@@ -171,6 +191,14 @@ class QuickBot():
     # Methods
     def run(self):
         global RUN_FLAG
+
+        # start up pru taking encoder readings
+        pp.start_up_pru()
+
+        adj = 0.014;
+        self.sysStartTime = time.time() + adj
+        self.pruCurrentTime = self.sysStartTime
+
         
         try:
             while RUN_FLAG is True:
@@ -307,14 +335,78 @@ class QuickBot():
 
     def readEncoderValues(self):
         print 'readEncodervalues called'
-            # State Encoder
-        self.encTime = [1,1]
-        self.encPos = [1,1]
-        self.encVel = [1,1]
+        
+        buffer = pp.get_pru_data()
+
+        #update bufferLoc
+        self.prevBufferLoc =  self.bufferLoc
+        self.bufferLoc = buffer[0,0]
+
+        # get new entries in buffer
+        if self.bufferLoc > self.prevBufferLoc:
+            print 'forwards from: ' + str(self.prevBufferLoc) +' to: ' + str(self.bufferLoc)
+            lenNE = self.bufferLoc - self.prevBufferLoc
+            newEntries = np.zeros((2,lenNE))
+            newEntries[0] = buffer[0,self.prevBufferLoc: self.bufferLoc]
+            newEntries[1] = buffer[1,self.prevBufferLoc: self.bufferLoc]
+
+        else:
+            print 'backwards from: ' + str(self.prevBufferLoc) +' back to: ' + str(self.bufferLoc)
+            lenNE = MAP_SIZE - self.prevBufferLoc + self.bufferLoc - 4
+            newEntries = np.zeros((2,lenNE))
+            newEntries[0] = np.concatenate(( buffer[0,self.prevBufferLoc:MAP_SIZE] , buffer[0,4:self.bufferLoc] ) , axis=1)
+            newEntries[1] = np.concatenate(( buffer[1,self.prevBufferLoc:MAP_SIZE] , buffer[1,4:self.bufferLoc] ) , axis=1)
+
+	
+        # update times
+
+        # pru predicted:
+        self.pruPrevTime = self.pruCurrentTime
+#       self.pruCurrentTime = self.pruPrevTime + len(newEntries_0)*0.0001
+        self.pruCurrentTime = self.pruPrevTime + lenNE * 0.0001
+
+        self.pruTotalTime = self.pruCurrentTime - self.sysStartTime
+
+        # system actual
+        self.sysCurrentTime = time.time()
+        self.sysTotalTime = self.sysCurrentTime - self.sysStartTime
+
+        # pru diferences
+        sysToPru  = self.sysTotalTime  - self.pruTotalTime
+
+#       print '******* Timings *******'
+#       
+#       print 'pruTotalTime: ' + str(self.pruTotalTime)
+#       print 'sysTotalTime: ' + str(self.sysTotalTime)
+#       print 'sys to pru: ' + str(sysToPru)
 
 
+        # calculate number of ticks 1->0 and 0->1 in newEntries
 
+        for side in range(0,2):
 
+            # calculate number of ticks in this sample
+            ticks = np.diff(newEntries[side])
+            ticks = abs(ticks)
+            ticks = sum(ticks)
+            if self.prevLastTick[side] != newEntries[side][0]: # catch ticks that go accross sets of samples
+                ticks = ticks + 1
+
+            self.prevLastTick[side] = newEntries[side][lenNE-1]
+
+            #calculate time elapsed for this set of samples         
+            timeElapsed = self.pruCurrentTime - self.pruPrevTime
+            self.tickVel[side] = ticks / timeElapsed
+
+            #create running average of tick vels over 5 readings 
+            self.runningAverage[side] = (self.runningAverage[side]*4 + self.tickVel[side])/5
+
+            # set State Encoder
+            self.encTime[side]  = 0.0                       # not required in modified QB program
+            self.encPos[side] = self.encPos[side] + (ticks * np.sign(self.pwm[side]))   # new  encoder tick position
+            self.encVel[side] = self.runningAverage[side] * np.sign(self.pwm[side]) # new encoder tick velocity
+
+            print 'encPos[{:d}]: {:0.1f}'.format(side,self.encPos[side]) + ' encVel[{:d}]: {:0.1f}'.format(side,self.encVel[side])
 
 
 
