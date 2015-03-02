c7866fb299fee730892f50f204ad61627c8d1660
root <root@beaglebone.localdomain>
2015-03-02 01:39:15 +0000

    pru data to np ndarray of int  working

--- pypru.c
+++ pypru.c
@@ -30,26 +30,46 @@
 // returns an array
 static PyObject* get_pru_data(PyObject* self, PyObject* args)
 {
-
 	// Allocate a C array (c_arr) to hold contents of Pru memory, obtained via read_pru_memory()
         char *c_arr;
+	int current_mem_loc;
 	c_arr = malloc(sizeof(char)*4096);
-        read_pru_memory(c_arr);
+        current_mem_loc = read_pru_memory(c_arr);
 
+//        printf("current memory location: %x\n",current_mem_loc);
+	
 	// Create a python array (p_arr) consisting of doubles to store the data from c_arr
         PyArrayObject* p_arr;
         int dims[1];
         dims[0] = MAP_SIZE;
-        p_arr = (PyArrayObject*) PyArray_FromDims(1, dims, 'd');
-        if(!p_arr) return 0;
+        p_arr = (PyArrayObject*) PyArray_FromDims(1, dims, 'i');
+        if(!p_arr) 
+	{
+		printf("p_arr not created\n");		
+		return 0;
+	}
 
 	// Create a pointer to the data in the p_arr (p_arr_data) and transfer data from  c_arr to p_arr_data
-	double  *p_arr_data;
-        int i;
-        p_arr_data = (double*) p_arr->data;
-        for(i=0; i<5; ++i)
-        p_arr_data[i] = c_arr[i];
+	int  *p_arr_data;
+        p_arr_data = (int*)p_arr->data;
 
+	p_arr_data[0] = current_mem_loc;
+	p_arr_data[1] = 0xFFFFFFFF;
+	p_arr_data[2] = 0xFFFFFFFF;
+	p_arr_data[3] = 0xFFFFFFFF;
+
+        int i;
+        for(i=4; i<current_mem_loc; ++i) 
+	{
+		p_arr_data[i] = c_arr[i];	
+	}
+//	printf("*******************************************************************\n");
+
+/*	for (i= 0;i < current_mem_loc;i++)
+	{
+		printf("p_arr_data[%d]: %d\n",i, p_arr_data[i]);
+	}
+*/
 	// free c_arr
 	free(c_arr);
 
@@ -62,40 +82,33 @@ int read_pru_memory(char* dataArray)
 {
         // declare variables
 
-        int fd;
-        void *map_base, *virt_addr, *rel_addr;
         off_t target; 
-        unsigned long current_mem_loc;
-        unsigned long b_len;
 
         printf("BB_MEM_LOC: %x\n", BBB_MEM_LOC);
 
-
-        // open the memory on BBB
-
-        if((fd = open("/dev/mem", O_RDWR | O_SYNC)) == -1) FATAL; // MB opens memory area to read/write to
+	// MB opens memory area to read/write to referenced by fd
+        int fd;
+        if((fd = open("/dev/mem", O_RDWR | O_SYNC)) == -1) FATAL; 
         printf("/dev/mem opened.\n"); 
         fflush(stdout);
 
-        // establish the base address  of /dev/mem which displays memory address BBB_MEM_LOC which equates to PRU Memory 
+        // establish the base address (map_base) of /dev/mem which displays memory address BBB_MEM_LOC which equates to PRU Memory 
+        void *map_base;
         map_base = mmap(0, MAP_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, BBB_MEM_LOC & ~MAP_MASK);
-
         if(map_base == (void *) -1) FATAL;
         printf("Memory mapped at address %p.\n", map_base); 
         fflush(stdout);
 
+
+	// sets up pointer for begining of pru memory in dev/mem/ (virt_addr) and for a relative address (rel_addr)
+        void  *virt_addr, *rel_addr;
         virt_addr = map_base  + (BBB_MEM_LOC & MAP_MASK);
+        
+	unsigned long current_mem_loc;
+        current_mem_loc  = *((unsigned long *) virt_addr); // set current_mem_loc to point to the address at the first 4 bytes (unsigned long) of pru memory 
 
-        current_mem_loc  = *((unsigned long *) virt_addr);
-
-        printf("current memory location: %x\n",current_mem_loc);
-
-
-        int i=0;
+	int i;
 	int temp;
-        dataArray[i]  = *((unsigned long *) virt_addr );
-        printf("dataArray[%x]: %x\n", i, dataArray[i]);
-
         for(i = 4; i< current_mem_loc; i++)
         {
                 rel_addr = virt_addr + i;
@@ -107,9 +120,9 @@ int read_pru_memory(char* dataArray)
                 {
                         dataArray[i] = 0;
                 }
-                printf("dataArray[%x]: %x\n", i, dataArray[i]);
+//                printf("dataArray[%x]: %x\n", i, dataArray[i]);
         }
-        return 1234;
+        return current_mem_loc;
 }
 
 
---
+++ run.py
@@ -0,0 +1,7 @@
+import pypru as p
+import numpy as np
+
+a = p.get_pru_data()
+
+print a[0:100]
+print a.dtype
